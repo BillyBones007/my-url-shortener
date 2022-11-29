4a85cba16767585a2ecaf45d2a939a08fb56f102
BillyBones007 <dskononov@yandex.ru>
2022-11-29 21:23:57 +0500

    iter3

---
+++ internal/routers/routers.go
@@ -0,0 +1,22 @@
+package routers
+
+import (
+	"github.com/BillyBones007/my-url-shortener/internal/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func NewRouter() chi.Router {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/", func(r chi.Router) {
+		r.Get("/{shortUrl}", handlers.ShortUrlHandler)
+		r.Post("/", handlers.ShortUrlHandler)
+	})
+
+	return r
+}
---
+++ internal/routers/routers_test.go
@@ -0,0 +1,65 @@
+package routers
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type returnData struct {
+	statusCode int
+	body       string
+	location   string
+}
+
+var rd returnData
+
+func testRequest(t *testing.T, ts *httptest.Server, method, endPoint string, body string, rd *returnData) {
+	switch method {
+	case "POST":
+		req, err := http.NewRequest(http.MethodPost, ts.URL+endPoint, strings.NewReader(body))
+		require.NoError(t, err)
+
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+
+		bodyResp, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+
+		defer resp.Body.Close()
+
+		rd.statusCode = resp.StatusCode
+		rd.body = string(bodyResp)
+
+	case "GET":
+		req, err := http.NewRequest(http.MethodGet, rd.body, nil)
+		require.NoError(t, err)
+
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+
+		rd.statusCode = resp.StatusCode
+		rd.location = resp.Header.Get("Location")
+	}
+}
+
+func TestRouter(t *testing.T) {
+	r := NewRouter()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	longUrl := "https://habr.com/ru/post/702374/"
+
+	testRequest(t, ts, "POST", "", longUrl, &rd)
+	assert.Equal(t, http.StatusCreated, rd.statusCode)
+
+	testRequest(t, ts, "GET", rd.body, "", &rd)
+	assert.Equal(t, http.StatusTemporaryRedirect, rd.statusCode)
+	assert.Equal(t, longUrl, rd.location)
+
+}
